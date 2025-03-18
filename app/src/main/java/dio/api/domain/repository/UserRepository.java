c4e2950ab39efd61eb688997179a83f8e6f1d9ff
higorv10 <4hv@protonmail.com>
2025-03-18 17:16:27 -0300

    Api-1

---
+++ app/src/main/java/dio/api/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package dio.api.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dio.api.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+}
